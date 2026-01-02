f9e89e7c19d527c68a35442e75ca8162e559a669
Le Tyrant Nolan <nolanletyrant@gmail.com>
2026-01-02 23:16:57 +0100

    Delete src/physics/integrators.py

--- src/physics/integrators.py
+++
@@ -1,7 +0,0 @@
-def rk4_step(f, x, u, dt, params):  
-  k1 = f(x, u, params)
-  k2 = f(x + 0.5 * dt * k1, u, params)
-  k3 = f(x + 0.5 * dt * k2, u, params)
-  k4 = f(x + dt * k3, u, params)
-
-  return x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
